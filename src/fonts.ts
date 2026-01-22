89f171d208010f93f79b509612a218a7943d6292
blck-snwmn <whitesnowmancreator@gmail.com>
2026-01-23 01:04:10 +0900

    Remove monolithic sample for modular rewrite

--- src/fonts.ts
+++
@@ -1,6 +0,0 @@
-import { loadFont } from '@remotion/google-fonts/NotoSansJP';
-
-export const { fontFamily } = loadFont('normal', {
-    weights: ['400', '700'],
-    subsets: ['latin', 'latin-ext'],
-});
